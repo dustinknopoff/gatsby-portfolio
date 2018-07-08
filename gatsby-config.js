7f637c70ebf221d7eacf1ab4a5a2f21e3b0edbea
Dustin <dustinknopoff@gmail.com>
2018-07-08 16:44:35 -0400

    content folder added

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
     subtitle: "Computer Science/Design Major at Northeastern University"
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content`,
+        name: `projects`
+      }
+    },
     `gatsby-plugin-glamor`,
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-react-helmet`
