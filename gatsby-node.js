c4e585ce1a200492f82248ebb06a9233a884fafa
Dustin <dustinknopoff@gmail.com>
2018-08-26 22:15:59 -0400

    spring cleaning

--- gatsby-node.js
+++ gatsby-node.js
@@ -58,34 +58,3 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
     });
   });
 };
-
-// exports.createPages = ({ graphql, boundActionCreators }) => {
-//   const { createPage } = boundActionCreators;
-//   return new Promise((resolve, reject) => {
-//     graphql(`
-//       {
-//         allMarkdownRemark {
-//           edges {
-//             node {
-//               fields {
-//                 slug
-//               }
-//             }
-//           }
-//         }
-//       }
-//     `).then(result => {
-//       result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-//         createPage({
-//           path: node.fields.slug,
-//           component: path.resolve(`./src/templates/post.js`),
-//           context: {
-//             // Data passed to context is available in page queries as GraphQL variables.
-//             slug: node.fields.slug
-//           }
-//         });
-//       });
-//       resolve();
-//     });
-//   });
-// };
