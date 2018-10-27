7aea0cfe6638ead62d835d1939f4b1f0e53659ae
Dustin Knopoff <dustinknopoff@gmail.com>
2018-10-26 23:00:19 -0400

    switch to feather icons

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,6 +5,7 @@ import Helmet from "react-helmet";
 import "prismjs/themes/prism-tomorrow.css";
 import { StaticQuery, graphql } from "gatsby";
 import Obfuscate from "react-obfuscate";
+import { Mail, GitHub, Linkedin, Rss } from "react-feather";
 
 export default ({ children }) => (
   <StaticQuery
@@ -40,8 +41,7 @@ export default ({ children }) => (
             Image by{" "}
             <a href="https://unsplash.com/photos/SMJC_JJm7W4">rawpixel</a>
             {", "}
-            and icons from{" "}
-            <a href="https://fontawesome.com/license">fontawesome</a>
+            and icons from <a href="https://feathericons.com">feather</a>
           </span>
           <nav>
             <ul>
@@ -67,23 +67,17 @@ export default ({ children }) => (
               />
             </Link>
             <Obfuscate email="dustinknopoff@gmail.com">
-              <img
-                alt="envelope icon"
-                src="https://res.cloudinary.com/dknopoff/image/upload/c_scale,f_auto,h_30/v1534990677/portfolio/envelope-solid.svg"
-              />
+              <Mail />
             </Obfuscate>
             <a href="https://github.com/dustinknopoff">
-              <img
-                alt="github icon"
-                src="https://res.cloudinary.com/dknopoff/image/upload/c_scale,f_auto,h_30/v1534991267/portfolio/github-brands.svg"
-              />
+              <GitHub />
             </a>
             <a href="https://www.linkedin.com/in/dustinknopoff">
-              <img
-                alt="linkedin icon"
-                src="https://res.cloudinary.com/dknopoff/image/upload/c_scale,f_auto,h_30/v1534991397/portfolio/linkedin-brands.svg"
-              />
+              <Linkedin />
             </a>
+            <Link to="rss.xml">
+              <Rss />
+            </Link>
           </div>
         </header>
         {children}
